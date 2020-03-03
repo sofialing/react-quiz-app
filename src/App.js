cbdddf833a34b0c922284928c06dd17299d7b33d
Sofia Ling <sofialing@live.se>
2020-03-03 14:23:39 +0100

    Deleted files

--- src/App.js
+++ src/App.js
@@ -1,26 +1,19 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+import { Route, BrowserRouter, Switch } from 'react-router-dom'
+import Start from './components/Start'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+	render() {
+		return (
+			<BrowserRouter>
+				<div id='App' className='container my-5'>
+					<Switch>
+						<Route exact path='/' component={Start} />
+					</Switch>
+				</div>
+			</BrowserRouter>
+		)
+	}
 }
 
-export default App;
+export default App
