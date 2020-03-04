48d7682b4395af1889e430345951dde26fede997
Sofia Ling <sofialing@live.se>
2020-03-04 13:58:52 +0100

    QG9-27 - created a submit function

--- src/components/Quiz.js
+++ src/components/Quiz.js
@@ -1,10 +1,13 @@
 import React, { Component } from 'react'
 import QuizQuestion from './QuizQuestion'
 import { db } from '../modules/firebase'
+import { Redirect } from 'react-router-dom'
 
 class Quiz extends Component {
 	state = {
-		quiz: null
+		quiz: null,
+		quizOver: false,
+		score: 0
 	}
 
 	getQuiz = () => {
@@ -27,7 +30,28 @@ class Quiz extends Component {
 		this.getQuiz()
 	}
 
+	submitQuiz = e => {
+		e.preventDefault()
+		this.setState({ quizOver: true })
+	}
+
 	render() {
+		if (this.state.quizOver) {
+			return (
+				<Redirect
+					to={{
+						pathname: '/result',
+						state: {
+							result: {
+								score: this.state.score,
+								name: this.state.name
+							}
+						}
+					}}
+				/>
+			)
+		}
+
 		const title = this.state.quiz ? this.state.quiz.name : ''
 		const quiz = this.state.quiz
 			? this.state.quiz.quiz.map((q, i) => (
@@ -43,8 +67,10 @@ class Quiz extends Component {
 		return (
 			<div className='container'>
 				<h1 className='text-center mb-5'>{title}</h1>
-				{quiz}
-				<button className='btn btn-primary'>Submit</button>
+				<form onSubmit={this.submitQuiz}>
+					{quiz}
+					<button className='btn btn-primary'>Submit</button>
+				</form>
 			</div>
 		)
 	}
