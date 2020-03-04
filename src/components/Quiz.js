f1e9ada13ecaefddf3b964c9f3ac73989bb5d8ec
samer munawwar <samer.mnr@gmail.com>
2020-03-04 19:13:32 +0100

    added the getMaxScore function

--- src/components/Quiz.js
+++ src/components/Quiz.js
@@ -1,66 +1,69 @@
-import React, { Component } from 'react';
-import QuizQuestion from './QuizQuestion';
-import { db } from '../modules/firebase';
+import React, { Component } from "react";
+import QuizQuestion from "./QuizQuestion";
+import { db } from "../modules/firebase";
 
 class Quiz extends Component {
-	state = {
-		quiz: null
-	};
+  state = {
+    quiz: null
+  };
 
-	getQuiz = () => {
-		db.collection('quizzes')
-			.doc(this.props.match.params.id)
-			.get()
-			.then(doc => {
-				if (doc.exists) {
-					this.setState({
-						quiz: { ...doc.data() }
-					});
-					// console.log(this.state.quiz);
-					// this.getMaxScore();
-				}
-			})
-			.catch(error => {
-				console.log('Error getting document:', error);
-			});
-	};
+  getQuiz = () => {
+    db.collection("quizzes")
+      .doc(this.props.match.params.id)
+      .get()
+      .then(doc => {
+        if (doc.exists) {
+          this.setState({
+            quiz: { ...doc.data() }
+          });
+          // console.log(this.state.quiz);
+        }
+      })
+      .catch(error => {
+        console.log("Error getting document:", error);
+      });
+  };
 
-	componentDidMount() {
-		this.getQuiz();
-	}
+  componentDidMount() {
+    this.getQuiz();
+  }
 
-	// getMaxScore = () => {
-	// 	//loopa igenom quiz och räkna ut totalpoäng
-	// 	const result = this.state.quiz.quiz.map(q => {
-	// 		return q.correct;
-	// 	});
+  getMaxScore = () => {
+    //loopa igenom quiz och räkna ut totalpoäng
+    const result = this.state.quiz.quiz.map(q => {
+      return q.correct.map(p => {
+        return parseInt(p.point);
+      });
+    });
+    const oneResult = result.flat(1);
+    const points = oneResult.reduce((a, b) => {
+      return a + b;
+    });
+    console.log(points);
+  };
 
-	// 	const totalResult = result.forEach(r => {
-	// 		console.log(r);
-	// 	});
-	// };
+  render() {
+    const title = this.state.quiz ? this.state.quiz.name : "";
+    const quiz = this.state.quiz
+      ? this.state.quiz.quiz.map((q, i) => (
+          <QuizQuestion
+            key={i}
+            question={q.question}
+            correct={q.correct}
+            wrong={q.wrong}
+          />
+        ))
+      : "";
 
-	render() {
-		const title = this.state.quiz ? this.state.quiz.name : '';
-		const quiz = this.state.quiz
-			? this.state.quiz.quiz.map((q, i) => (
-					<QuizQuestion
-						key={i}
-						question={q.question}
-						correct={q.correct}
-						wrong={q.wrong}
-					/>
-			  ))
-			: '';
-
-		return (
-			<div className='container'>
-				<h1 className='text-center mb-5'>{title}</h1>
-				{quiz}
-				<button className='btn btn-primary'>Submit</button>
-			</div>
-		);
-	}
+    return (
+      <div className="container">
+        <h1 className="text-center mb-5">{title}</h1>
+        {quiz}
+        <button className="btn btn-primary">Submit</button>
+        <span onClick={this.getMaxScore}>click</span>
+      </div>
+    );
+  }
 }
 
 export default Quiz;
