6777f99b7cbc2394f30592cc428f93f2350ad361
Lina Väppling <lina.wappling@gmail.com>
2020-03-04 10:37:09 +0100

    QG9-19 added quizfrom database

--- src/App.js
+++ src/App.js
@@ -1,19 +1,21 @@
-import React, { Component } from 'react'
-import { Route, BrowserRouter, Switch } from 'react-router-dom'
-import Start from './components/Start'
+import React, { Component } from "react";
+import { Route, BrowserRouter, Switch } from "react-router-dom";
+import Start from "./components/Start";
+import Result from "./components/Result";
 
 class App extends Component {
-	render() {
-		return (
-			<BrowserRouter>
-				<div id='App' className='container my-5'>
-					<Switch>
-						<Route exact path='/' component={Start} />
-					</Switch>
-				</div>
-			</BrowserRouter>
-		)
-	}
+    render() {
+        return (
+            <BrowserRouter>
+                <div id='App' className='container my-5'>
+                    <Switch>
+                        <Route exact path='/' component={Start} />
+                        <Route path='/result' component={Result} />
+                    </Switch>
+                </div>
+            </BrowserRouter>
+        );
+    }
 }
 
-export default App
+export default App;
