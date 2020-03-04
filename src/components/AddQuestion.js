ca5c9819328b388c85858273ef3516b4845ed556
samer munawwar <samer.mnr@gmail.com>
2020-03-04 10:35:35 +0100

    Skapa en funktion för att lägga till flera input-fält för att skapa fler svarsalternativ

--- src/components/AddQuestion.js
+++ src/components/AddQuestion.js
@@ -7,7 +7,9 @@ class AddQuestion extends React.Component {
     question: "",
     correctAnswer: "",
     point: "",
-    wrongAnswer: ""
+    wrongAnswer: "",
+    wrongAnswerArr: [],
+    correctAnswerArr: []
   };
 
   handleChange = e => {
@@ -21,65 +23,88 @@ class AddQuestion extends React.Component {
     let questionForm = {};
 
     console.log(questionForm);
+    console.log(this.state.wrongAnswerArr);
 
     db.collection("quizzes")
       .doc("eBhOLLIzCGn2J6cDpOCe")
       .update({
         quiz: firebase.firestore.FieldValue.arrayUnion({
-          correct: [
-            { answer: this.state.correctAnswer, point: this.state.point }
-          ],
+          correct: [...this.state.correctAnswerArr],
           question: this.state.question,
-          wrong: [this.state.wrongAnswer]
+          wrong: [...this.state.wrongAnswerArr]
         })
       });
   };
 
+  handleIncorrectAnswer = () => {
+    this.setState({
+      wrongAnswerArr: [...this.state.wrongAnswerArr, this.state.wrongAnswer]
+    });
+  };
+
+  handleCorrectAnswer = () => {
+    console.log(this.state.correctAnswerArr);
+    this.setState({
+      correctAnswerArr: [
+        ...this.state.correctAnswerArr,
+        { answer: this.state.correctAnswer, point: this.state.point }
+      ]
+    });
+  };
+
   render() {
     return (
-      <form>
-        <div className="form-group">
-          <label>Write a question</label>
-          <input
-            type="text"
-            className="form-control"
-            placeholder="Write a question"
-            name="question"
-            onChange={e => this.handleChange(e)}
-          />
-        </div>
-        <div className="form-group">
-          <label>The correct answer</label>
-          <input
-            type="text"
-            className="form-control"
-            placeholder="Write the correct answer"
-            name="correctAnswer"
-            onChange={e => this.handleChange(e)}
-          />
-        </div>
-        <div className="form-group">
-          <label>The correct answer point</label>
-          <input
-            type="text"
-            className="form-control"
-            placeholder="write a point"
-            name="point"
-            onChange={e => this.handleChange(e)}
-          />
-        </div>
-        <div className="form-group">
-          <label>The Wrong answer</label>
-          <input
-            type="text"
-            className="form-control"
-            placeholder="The Wrong answer"
-            name="wrongAnswer"
-            onChange={e => this.handleChange(e)}
-          />
-        </div>
-        <button onClick={e => this.handleAddQu(e)}>ADD</button>
-      </form>
+      <div>
+        <form>
+          <div className="form-group">
+            <label>Write a question</label>
+            <input
+              type="text"
+              className="form-control"
+              placeholder="Write a question"
+              name="question"
+              onChange={e => this.handleChange(e)}
+            />
+          </div>
+          <div className="form-group">
+            <label>The correct answer</label>
+            <input
+              type="text"
+              className="form-control"
+              placeholder="Write the correct answer"
+              name="correctAnswer"
+              onChange={e => this.handleChange(e)}
+            />
+          </div>
+          <div className="form-group">
+            <label>The correct answer point</label>
+            <input
+              type="text"
+              className="form-control"
+              placeholder="write a point"
+              name="point"
+              onChange={e => this.handleChange(e)}
+            />
+          </div>
+          <div className="form-group">
+            <label>The Wrong answer</label>
+            <input
+              type="text"
+              className="form-control"
+              placeholder="The Wrong answer"
+              name="wrongAnswer"
+              onChange={e => this.handleChange(e)}
+            />
+          </div>
+          <button onClick={e => this.handleAddQu(e)}>ADD</button>
+        </form>
+        <button onClick={this.handleIncorrectAnswer}>
+          Add more incorrect Answer
+        </button>
+        <button onClick={this.handleCorrectAnswer}>
+          Add more correct Answer
+        </button>
+      </div>
     );
   }
 }
