96f686738935d7aed4054d432ee7c35f8077da64
Sofia Ling <sofialing@live.se>
2020-03-04 11:42:58 +0100

    QG9-24 Function to get a specific quiz

--- src/components/QuizQuestion.js
+++ src/components/QuizQuestion.js
@@ -1,23 +1,21 @@
-import React from "react";
-import QuizOptions from "./QuizOptions";
+import React from 'react'
+import QuizOptions from './QuizOptions'
 
 const QuizQuestion = props => {
-  const { question } = props;
-  const options = props.wrong;
-  console.log(props);
-  const correct = props.correct.map(c => {
-    console.log(c.answer);
-    return c;
-  });
-  return (
-    <div className="card mb-4">
-      <div className="card-body">
-        <h5 className="card-title">{question}</h5>
-        <QuizOptions options={options} correct={correct} />
-        {/* <QuizOptions options={options} /> */}
-      </div>
-    </div>
-  );
-};
+	const { question } = props
+	const options = props.wrong
+	const correct = props.correct.map(c => {
+		return c
+	})
 
-export default QuizQuestion;
+	return (
+		<div className='card mb-4'>
+			<div className='card-body'>
+				<h5 className='card-title'>{question}</h5>
+				<QuizOptions options={options} correct={correct} />
+			</div>
+		</div>
+	)
+}
+
+export default QuizQuestion
