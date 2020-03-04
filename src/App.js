c12eaafe8d5338cd21b08ef1a8f8d2e69f09dfd9
samer munawwar <samer.mnr@gmail.com>
2020-03-04 14:29:28 +0100

    funktion som lägger till ett nytt quiz i firestore +Flera alternativ ska kunna vara rätt

--- src/App.js
+++ src/App.js
@@ -3,23 +3,23 @@ import { Route, BrowserRouter, Switch } from "react-router-dom";
 import Start from "./components/Start";
 import Result from "./components/Result";
 import Quiz from "./components/Quiz";
-import AddQuestion from "./components/AddQuestion";
+import AddNewQuiz from "./components/AddNewQuiz";
 
 class App extends Component {
-    render() {
-        return (
-            <BrowserRouter>
-                <div id='App' className='container my-5'>
-                    <Switch>
-                        <Route exact path='/' component={Start} />
-                        <Route path='/quiz' component={Quiz} />
-                        <Route path='/add' component={AddQuestion} />
-                        <Route path='/result' component={Result} />
-                    </Switch>
-                </div>
-            </BrowserRouter>
-        );
-    }
+  render() {
+    return (
+      <BrowserRouter>
+        <div id="App" className="container my-5">
+          <Switch>
+            <Route exact path="/" component={Start} />
+            <Route path="/quiz" component={Quiz} />
+            <Route path="/result" component={Result} />
+            <Route path="/addQuiz" component={AddNewQuiz} />
+          </Switch>
+        </div>
+      </BrowserRouter>
+    );
+  }
 }
 
 export default App;
