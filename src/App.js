69a72c325b4deca96e97796261d58b91fb57e23c
samer munawwar <samer.mnr@gmail.com>
2020-03-05 14:15:42 +0100

    fixed App conflict

--- src/App.js
+++ src/App.js
@@ -1,27 +1,27 @@
-import React, { Component } from 'react'
-import { Route, BrowserRouter, Switch } from 'react-router-dom'
-import Start from './components/Start'
-import Quiz from './components/Quiz'
-import AddNewQuiz from './components/AddNewQuiz'
-import Navbar from './components/Navbar'
+import React, { Component } from "react";
+import { Route, BrowserRouter, Switch } from "react-router-dom";
+import Start from "./components/Start";
+import Quiz from "./components/Quiz";
+import AddNewQuiz from "./components/AddNewQuiz";
+import Navbar from "./components/Navbar";
 
 class App extends Component {
-	render() {
-		return (
-			<BrowserRouter>
-				<div id='App'>
-					<Navbar />
-					<div className='container my-5'>
-						<Switch>
-							<Route exact path='/' component={Start} />
-							<Route path='/quiz/:id' component={Quiz} />
-							<Route path='/addQuiz' component={AddNewQuiz} />
-						</Switch>
-					</div>
-				</div>
-			</BrowserRouter>
-		)
-	}
+  render() {
+    return (
+      <BrowserRouter>
+        <div id="App">
+          <Navbar />
+          <div className="container my-5">
+            <Switch>
+              <Route exact path="/" component={Start} />
+              <Route path="/quiz/:id" component={Quiz} />
+              <Route path="/addQuiz" component={AddNewQuiz} />
+            </Switch>
+          </div>
+        </div>
+      </BrowserRouter>
+    );
+  }
 }
 
-export default App
+export default App;
