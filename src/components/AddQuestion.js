0cca7166314e79c228592f8321eb422d2f7cab25
samer munawwar <samer.mnr@gmail.com>
2020-03-05 16:45:44 +0100

    Tömma input-fält

--- src/components/AddQuestion.js
+++ src/components/AddQuestion.js
@@ -4,173 +4,198 @@ import firebase from "firebase/app";
 import { Link } from "react-router-dom";
 
 class AddQuestion extends React.Component {
-  state = {
-    question: "",
-    correctAnswer: "",
-    point: "",
-    wrongAnswer: "",
-    wrongAnswerArr: [],
-    correctAnswerArr: []
-  };
+    state = {
+        question: "",
+        correctAnswer: "",
+        point: "",
+        wrongAnswer: "",
+        wrongAnswerArr: [],
+        correctAnswerArr: []
+    };
 
-  handleChange = e => {
-    this.setState({
-      [e.target.name]: e.target.value
-    });
-  };
+    handleChange = e => {
+        this.setState({
+            [e.target.name]: e.target.value
+        });
+    };
 
-  handleAddQu = (e, id) => {
-    e.preventDefault();
-    // let questionForm = {};
+    handleAddQu = (e, id) => {
+        e.preventDefault();
+        if (this.state.question.length < 6) return;
+        console.log(this.state.wrongAnswerArr);
 
-    // console.log(questionForm);
-    console.log(this.state.wrongAnswerArr);
+        db.collection("quizzes")
+            .doc(id)
+            .update({
+                quiz: firebase.firestore.FieldValue.arrayUnion({
+                    correct: [...this.state.correctAnswerArr],
+                    question: this.state.question,
+                    wrong: [...this.state.wrongAnswerArr]
+                })
+            });
+        this.setState({
+            question: "",
+            correctAnswer: "",
+            point: "",
+            wrongAnswer: "",
+            wrongAnswerArr: [],
+            correctAnswerArr: []
+        });
+    };
 
-    db.collection("quizzes")
-      .doc(id)
-      .update({
-        quiz: firebase.firestore.FieldValue.arrayUnion({
-          correct: [...this.state.correctAnswerArr],
-          question: this.state.question,
-          wrong: [...this.state.wrongAnswerArr]
-        })
-      });
-    this.setState({
-      question: "",
-      correctAnswer: "",
-      point: "",
-      wrongAnswer: "",
-      wrongAnswerArr: [],
-      correctAnswerArr: []
-    });
-  };
+    handleIncorrectAnswer = () => {
+        if (this.state.wrongAnswer.length < 3) return;
+        this.setState({
+            wrongAnswerArr: [
+                ...this.state.wrongAnswerArr,
+                this.state.wrongAnswer
+            ],
+            wrongAnswer: ""
+        });
+    };
 
-  handleIncorrectAnswer = () => {
-    this.setState({
-      wrongAnswerArr: [...this.state.wrongAnswerArr, this.state.wrongAnswer]
-    });
-  };
+    handleCorrectAnswer = () => {
+        console.log(this.state.correctAnswerArr);
+        if (
+            this.state.correctAnswer.length < 3 ||
+            this.state.point.length === 0
+        )
+            return;
+        this.setState({
+            correctAnswerArr: [
+                ...this.state.correctAnswerArr,
+                { answer: this.state.correctAnswer, point: this.state.point }
+            ],
+            correctAnswer: "",
+            point: ""
+        });
+    };
 
-  handleCorrectAnswer = () => {
-    console.log(this.state.correctAnswerArr);
-    this.setState({
-      correctAnswerArr: [
-        ...this.state.correctAnswerArr,
-        { answer: this.state.correctAnswer, point: this.state.point }
-      ]
-    });
-  };
+    handleCorrectDelete = i => {
+        let newCorrectAnswer = this.state.correctAnswerArr.filter(
+            (c, index) => {
+                return i !== index;
+            }
+        );
+        this.setState({
+            correctAnswerArr: newCorrectAnswer
+        });
+    };
 
-  handleCorrectDelete = i => {
-    let newCorrectAnswer = this.state.correctAnswerArr.filter((c, index) => {
-      return i !== index;
-    });
-    this.setState({
-      correctAnswerArr: newCorrectAnswer
-    });
-  };
+    handleWrongDelete = i => {
+        let newWrongAnswer = this.state.wrongAnswerArr.filter((c, index) => {
+            return i !== index;
+        });
+        this.setState({
+            wrongAnswerArr: newWrongAnswer
+        });
+    };
 
-  handleWrongDelete = i => {
-    let newWrongAnswer = this.state.wrongAnswerArr.filter((c, index) => {
-      return i !== index;
-    });
-    this.setState({
-      wrongAnswerArr: newWrongAnswer
-    });
-  };
-
-  render() {
-    return (
-      <div>
-        <form>
-          <div className="form-group">
-            <label>Write a question</label>
-            <input
-              type="text"
-              className="form-control"
-              placeholder="Write a question"
-              name="question"
-              onChange={e => this.handleChange(e)}
-              value={this.state.question}
-            />
-          </div>
-          <ul>
-            {this.state.correctAnswerArr.map((answer, i) => {
-              return (
-                <li key={i}>
-                  <span>
-                    {answer.answer} {answer.point}
-                  </span>
-                  <span onClick={() => this.handleCorrectDelete(i)}>X</span>
-                </li>
-              );
-            })}
-          </ul>
-          <div className="input-group mb-4">
-            <input
-              type="text"
-              className="form-control"
-              placeholder="Write the correct answer"
-              name="correctAnswer"
-              onChange={e => this.handleChange(e)}
-              value={this.state.correctAnswer}
-            />
-            <input
-              type="number"
-              className="form-control"
-              placeholder="write a point"
-              name="point"
-              onChange={e => this.handleChange(e)}
-              value={this.state.point}
-            />
-            <div className="input-group-prepend">
-              <span
-                className="input-group-text span-button"
-                onClick={this.handleCorrectAnswer}
-              >
-                Add more correct Answer
-              </span>
+    render() {
+        return (
+            <div>
+                <form>
+                    <div className="form-group">
+                        <label>Write a question</label>
+                        <input
+                            type="text"
+                            className="form-control"
+                            placeholder="Write a question"
+                            name="question"
+                            onChange={e => this.handleChange(e)}
+                            value={this.state.question}
+                        />
+                    </div>
+                    <ul>
+                        {this.state.correctAnswerArr.map((answer, i) => {
+                            return (
+                                <li key={i} className="flex-list">
+                                    <span>The answer: {answer.answer}</span>
+                                    <span>Points: {answer.point}</span>
+                                    <span
+                                        className="delete-span"
+                                        onClick={() =>
+                                            this.handleCorrectDelete(i)
+                                        }
+                                    >
+                                        X
+                                    </span>
+                                </li>
+                            );
+                        })}
+                    </ul>
+                    <div className="input-group mb-4">
+                        <input
+                            type="text"
+                            className="form-control"
+                            placeholder="Write the correct answer"
+                            name="correctAnswer"
+                            onChange={e => this.handleChange(e)}
+                            value={this.state.correctAnswer}
+                        />
+                        <input
+                            type="number"
+                            className="form-control"
+                            placeholder="write a point"
+                            name="point"
+                            onChange={e => this.handleChange(e)}
+                            value={this.state.point}
+                        />
+                        <div className="input-group-prepend">
+                            <span
+                                className="input-group-text span-button"
+                                onClick={this.handleCorrectAnswer}
+                            >
+                                Add more correct Answer
+                            </span>
+                        </div>
+                    </div>
+                    <ul>
+                        {this.state.wrongAnswerArr.map((answer, i) => {
+                            return (
+                                <li key={i} className="flex-list">
+                                    <span>The answer: {answer}</span>
+                                    <span
+                                        className="delete-span"
+                                        onClick={() =>
+                                            this.handleWrongDelete(i)
+                                        }
+                                    >
+                                        x
+                                    </span>
+                                </li>
+                            );
+                        })}
+                    </ul>
+                    <div className="input-group mb-3">
+                        <input
+                            type="text"
+                            className="form-control"
+                            placeholder="The Wrong answer"
+                            name="wrongAnswer"
+                            onChange={e => this.handleChange(e)}
+                            value={this.state.wrongAnswer}
+                        />
+                        <div className="input-group-prepend ">
+                            <span
+                                className="input-group-text span-button"
+                                onClick={this.handleIncorrectAnswer}
+                            >
+                                Add more incorrect Answer
+                            </span>
+                        </div>
+                    </div>
+                    <button
+                        type="submit"
+                        onClick={e => this.handleAddQu(e, this.props.id)}
+                    >
+                        ADD
+                    </button>
+                </form>
+                <Link to="">go to the quiz</Link>
             </div>
-          </div>
-          <ul>
-            {this.state.wrongAnswerArr.map((answer, i) => {
-              return (
-                <li key={i}>
-                  <span>{answer}</span>
-                  <span onClick={() => this.handleWrongDelete(i)}>x</span>
-                </li>
-              );
-            })}
-          </ul>
-          <div className="input-group mb-3">
-            <input
-              type="text"
-              className="form-control"
-              placeholder="The Wrong answer"
-              name="wrongAnswer"
-              onChange={e => this.handleChange(e)}
-              value={this.state.wrongAnswer}
-            />
-            <div className="input-group-prepend ">
-              <span
-                className="input-group-text span-button"
-                onClick={this.handleIncorrectAnswer}
-              >
-                Add more incorrect Answer
-              </span>
-            </div>
-          </div>
-          <button
-            type="submit"
-            onClick={e => this.handleAddQu(e, this.props.id)}
-          >
-            ADD
-          </button>
-        </form>
-        <Link to="">go to the quiz</Link>
-      </div>
-    );
-  }
+        );
+    }
 }
 
 export default AddQuestion;
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,46 +1,46 @@
-import React from 'react';
-import { Link, NavLink } from 'react-router-dom';
+import React from "react";
+import { Link, NavLink } from "react-router-dom";
 
 const Navbar = props => {
-	//lägg in när firebase auth är klart
-	// const handleSignOut = e => {
-	// 	auth.signOut().then(() => {
-	// 		//redirect to login
-	// 		console.log('sign out');
-	// 	});
-	// };
+    //lägg in när firebase auth är klart
+    // const handleSignOut = e => {
+    // 	auth.signOut().then(() => {
+    // 		//redirect to login
+    // 		console.log('sign out');
+    // 	});
+    // };
 
-	return (
-		<nav className='navbar navbar-expand navbar-light bg-light mb-3'>
-			<Link to='/' className='navbar-brand'>
-				Quiz
-			</Link>
-			<button
-				className='navbar-toggler'
-				type='button'
-				data-toggle='collapse'
-				data-target='#navbarNav'
-				aria-controls='navbarNav'
-				aria-expanded='false'
-				aria-label='Toggle navigation'
-			>
-				<span className='navbar-toggler-icon'></span>
-			</button>
-			<div className='collapse navbar-collapse' id='navbarNav'>
-				<ul className='navbar-nav ml-auto'>
-					<li className='nav-item'>
-						<NavLink to='/' className='nav-link'>
-							All Quizzes
-						</NavLink>
-					</li>
-					<li className='nav-item'>
-						<NavLink to='/addQuiz' className='nav-link'>
-							Add Quiz
-						</NavLink>
-					</li>
+    return (
+        <nav className="navbar navbar-expand navbar-light bg-light mb-3">
+            <Link to="/" className="navbar-brand">
+                Quiz
+            </Link>
+            <button
+                className="navbar-toggler"
+                type="button"
+                data-toggle="collapse"
+                data-target="#navbarNav"
+                aria-controls="navbarNav"
+                aria-expanded="false"
+                aria-label="Toggle navigation"
+            >
+                <span className="navbar-toggler-icon"></span>
+            </button>
+            <div className="collapse navbar-collapse" id="navbarNav">
+                <ul className="navbar-nav ml-auto">
+                    <li className="nav-item">
+                        <NavLink to="/" className="nav-link">
+                            All Quizzes
+                        </NavLink>
+                    </li>
+                    <li className="nav-item">
+                        <NavLink to="/addQuiz" className="nav-link">
+                            Add Quiz
+                        </NavLink>
+                    </li>
 
-					{/* byt till detta när firebase auth är klart */}
-					{/* {props.user ? (
+                    {/* byt till detta när firebase auth är klart */}
+                    {/* {props.user ? (
 							<li className='nav-item'>
 								<span
 									className='nav-link logout'
@@ -57,15 +57,15 @@ const Navbar = props => {
 							</li>
 						)} */}
 
-					<li className='nav-item'>
-						<NavLink to='/login' className='nav-link'>
-							Login
-						</NavLink>
-					</li>
-				</ul>
-			</div>
-		</nav>
-	);
+                    <li className="nav-item">
+                        <NavLink to="/login" className="nav-link">
+                            Login
+                        </NavLink>
+                    </li>
+                </ul>
+            </div>
+        </nav>
+    );
 };
 
 export default Navbar;
