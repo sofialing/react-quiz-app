2a58f460eeb5d3b43b4ce43768f501ca6e160fc0
Sofia Ling <sofialing@live.se>
2020-03-06 11:15:48 +0100

    QG9-46 - Minor changes in QuizOption component

--- src/components/QuizOptions.js
+++ src/components/QuizOptions.js
@@ -1,7 +1,6 @@
-import React, { Component } from "react";
+import React, { Component } from 'react'
 
 class QuizOptions extends Component {
-<<<<<<< HEAD
 	state = {
 		userAnswer: [],
 		message: false
@@ -74,62 +73,6 @@ class QuizOptions extends Component {
 			</form>
 		)
 	}
-=======
-  state = {
-    userAnswer: []
-  };
-
-  handleChange = e => {
-    const { name } = e.target;
-
-    if (this.state.userAnswer.includes(name)) {
-      const newArray = [...this.state.userAnswer];
-      const i = newArray.indexOf(name);
-      newArray.splice(i, 1);
-      this.setState({ userAnswer: newArray });
-    } else {
-      this.setState(prevState => ({
-        userAnswer: [...prevState.userAnswer, name]
-      }));
-    }
-  };
-
-  checkAnswer = e => {
-    e.preventDefault();
-    let point = 0;
-    if (this.state.userAnswer[0] === this.props.correct[0].answer) {
-      point = 1;
-    }
-
-    this.setState({ userAnswer: [] });
-    this.props.onUpdateScore(point);
-  };
-
-  render() {
-    const checkboxes = this.props.options.map((option, i) => (
-      <div className="custom-control custom-checkbox" key={i}>
-        <input
-          type="checkbox"
-          className="custom-control-input"
-          id={`${option}-${i}`}
-          name={option}
-          onChange={this.handleChange}
-          checked={this.state.userAnswer.includes(option)}
-        />
-        <label className="custom-control-label" htmlFor={`${option}-${i}`}>
-          {option}
-        </label>
-      </div>
-    ));
-
-    return (
-      <form onSubmit={this.checkAnswer}>
-        {checkboxes}
-        <button className="btn btn-primary mt-3">Next question</button>
-      </form>
-    );
-  }
->>>>>>> 840f58a2f4e779e6839451814664b19ad0d9125f
 }
 
-export default QuizOptions;
+export default QuizOptions
