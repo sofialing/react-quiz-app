c764c12e71a6b9027f9bb46e485d0b12627ac950
Sofia Ling <sofialing@live.se>
2020-03-06 14:16:42 +0100

    Fixed merge conflicts

--- src/components/Quiz.js
+++ src/components/Quiz.js
@@ -1,48 +1,47 @@
-import React, { Component } from "react";
-import QuizQuestion from "./QuizQuestion";
-import Result from "./Result";
-import { db } from "../modules/firebase";
-import { Link } from "react-router-dom";
+import React, { Component } from 'react'
+import QuizQuestion from './QuizQuestion'
+import Result from './Result'
+import { db } from '../modules/firebase'
+import { Link } from 'react-router-dom'
 
 class Quiz extends Component {
-    state = {
-        current: 0,
-        name: "",
-        quiz: null,
-        quizOver: false,
-        score: 0
-    };
+	state = {
+		current: 0,
+		name: '',
+		quiz: null,
+		quizOver: false,
+		score: 0
+	}
 
-    componentDidMount() {
-        this.getQuiz();
-    }
+	componentDidMount() {
+		this.getQuiz()
+	}
 
-    getQuiz = () => {
-        db.collection("quizzes")
-            .doc(this.props.match.params.id)
-            .get()
-            .then(doc => {
-                if (doc.exists) {
-                    this.setState({
-                        name: doc.data().name,
-                        quiz: [...doc.data().quiz]
-                    });
-                }
-            })
-            .catch(error => {
-                console.log("Error getting document:", error);
-            });
-    };
+	getQuiz = () => {
+		db.collection('quizzes')
+			.doc(this.props.match.params.id)
+			.get()
+			.then(doc => {
+				if (doc.exists) {
+					this.setState({
+						name: doc.data().name,
+						quiz: [...doc.data().quiz]
+					})
+				}
+			})
+			.catch(error => {
+				console.log('Error getting document:', error)
+			})
+	}
 
-    getMaxScore = () => {
-        const maxScore = this.state.quiz
-            .map(question => Number(question.point))
-            .reduce((total, point) => total + point);
+	getMaxScore = () => {
+		const maxScore = this.state.quiz
+			.map(question => Number(question.point))
+			.reduce((total, point) => total + point)
 
-        return maxScore;
-    };
+		return maxScore
+	}
 
-<<<<<<< HEAD
 	isLastQuestion = () => {
 		return this.state.current === this.state.quiz.length - 1
 	}
@@ -56,39 +55,27 @@ class Quiz extends Component {
 			this.setState({ quizOver: true })
 		}
 	}
-=======
-    showNextQuestion = () => {
-        if (this.state.current < this.state.quiz.length - 1) {
-            this.setState(prevState => ({
-                current: prevState.current + 1
-            }));
-        } else {
-            this.setState({ quizOver: true });
-        }
-    };
->>>>>>> master
 
-    UpdateScore = point => {
-        this.setState(prevState => ({
-            score: prevState.score + point
-        }));
-        this.showNextQuestion();
-    };
+	UpdateScore = point => {
+		this.setState(prevState => ({
+			score: prevState.score + point
+		}))
+		this.showNextQuestion()
+	}
 
-    render() {
-        if (this.state.quizOver) {
-            return (
-                <Result
-                    result={{
-                        name: this.state.name,
-                        score: this.state.score,
-                        maxScore: this.getMaxScore()
-                    }}
-                />
-            );
-        }
+	render() {
+		if (this.state.quizOver) {
+			return (
+				<Result
+					result={{
+						name: this.state.name,
+						score: this.state.score,
+						maxScore: this.getMaxScore()
+					}}
+				/>
+			)
+		}
 
-<<<<<<< HEAD
 		return this.state.quiz ? (
 			<div>
 				<h1 className='text-center mb-5'>{this.state.name}</h1>
@@ -105,23 +92,6 @@ class Quiz extends Component {
 			''
 		)
 	}
-=======
-        return this.state.quiz ? (
-            <div>
-                <h1 className="text-center mb-5">{this.state.name}</h1>
-                <QuizQuestion
-                    quiz={this.state.quiz[this.state.current]}
-                    onUpdateScore={this.UpdateScore}
-                />
-                <Link to="/" className="btn btn-primary">
-                    Back to all quizzes
-                </Link>
-            </div>
-        ) : (
-            ""
-        );
-    }
->>>>>>> master
 }
 
-export default Quiz;
+export default Quiz
--- src/components/QuizQuestion.js
+++ src/components/QuizQuestion.js
@@ -1,12 +1,11 @@
-import React from "react";
-import QuizOptions from "./QuizOptions";
+import React from 'react'
+import QuizOptions from './QuizOptions'
 
 const QuizQuestion = props => {
-    const { question, correct, wrong, point } = props.quiz;
-    const options = [...correct, ...wrong];
-    options.sort(() => 0.5 - Math.random());
+	const { question, correct, wrong, point } = props.quiz
+	const options = [...correct, ...wrong]
+	options.sort(() => 0.5 - Math.random())
 
-<<<<<<< HEAD
 	return (
 		<div className='card mb-4'>
 			<div className='card-body'>
@@ -22,21 +21,5 @@ const QuizQuestion = props => {
 		</div>
 	)
 }
-=======
-    return (
-        <div className="card mb-4">
-            <div className="card-body">
-                <h5 className="card-title">{question}</h5>
-                <QuizOptions
-                    options={options}
-                    correct={correct}
-                    onUpdateScore={props.onUpdateScore}
-                    point={point}
-                />
-            </div>
-        </div>
-    );
-};
->>>>>>> master
 
-export default QuizQuestion;
+export default QuizQuestion
