6ee317342ef50bca5179f94b2e810563d8023975
Lina Väppling <lina.wappling@gmail.com>
2020-03-06 15:09:46 +0100

    QG9-55 - Added Page not found component

--- src/App.js
+++ src/App.js
@@ -1,51 +1,53 @@
-import React, { Component } from "react";
-import { Route, BrowserRouter, Switch } from "react-router-dom";
-import Start from "./components/Start";
-import Quiz from "./components/Quiz";
-import AddNewQuiz from "./components/AddNewQuiz";
-import Navbar from "./components/Navbar";
-import Login from "./components/Login";
-import { auth } from "./modules/firebase";
-import SignUp from "./components/signup";
+import React, { Component } from 'react';
+import { Route, BrowserRouter, Switch } from 'react-router-dom';
+import Start from './components/Start';
+import Quiz from './components/Quiz';
+import AddNewQuiz from './components/AddNewQuiz';
+import Navbar from './components/Navbar';
+import Login from './components/Login';
+import { auth } from './modules/firebase';
+import SignUp from './components/signup';
+import NotFound from './components/NotFound';
 
 class App extends Component {
-    state = {
-        user: null
-    };
-    componentDidMount() {
-        auth.onAuthStateChanged(authUser => {
-            if (authUser) {
-                this.setState({
-                    user: {
-                        email: authUser.email
-                    }
-                });
-            } else {
-                this.setState({
-                    user: null
-                });
-            }
-        });
-    }
+	state = {
+		user: null
+	};
+	componentDidMount() {
+		auth.onAuthStateChanged(authUser => {
+			if (authUser) {
+				this.setState({
+					user: {
+						email: authUser.email
+					}
+				});
+			} else {
+				this.setState({
+					user: null
+				});
+			}
+		});
+	}
 
-    render() {
-        return (
-            <BrowserRouter>
-                <div id="App">
-                    <Navbar user={this.state.user} />
-                    <div className="container my-5">
-                        <Switch>
-                            <Route exact path="/" component={Start} />
-                            <Route path="/quiz/:id" component={Quiz} />
-                            <Route path="/addQuiz" component={AddNewQuiz} />
-                            <Route path="/login" component={Login} />
-                            <Route path="/signUp" component={SignUp} />
-                        </Switch>
-                    </div>
-                </div>
-            </BrowserRouter>
-        );
-    }
+	render() {
+		return (
+			<BrowserRouter>
+				<div id='App'>
+					<Navbar user={this.state.user} />
+					<div className='container my-5'>
+						<Switch>
+							<Route exact path='/' component={Start} />
+							<Route path='/quiz/:id' component={Quiz} />
+							<Route path='/addQuiz' component={AddNewQuiz} />
+							<Route path='/login' component={Login} />
+							<Route path='/signUp' component={SignUp} />
+							<Route component={NotFound} />
+						</Switch>
+					</div>
+				</div>
+			</BrowserRouter>
+		);
+	}
 }
 
 export default App;
---
+++ src/components/NotFound.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const NotFound = () => {
+	return (
+		<div>
+			<h3 className='text-center'>
+				The page your are trying to enter does not exist
+			</h3>
+		</div>
+	);
+};
+
+export default NotFound;
