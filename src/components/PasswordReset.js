11bc566af8c6c67c4eeca02c54dc8a3dfd774ccf
samer munawwar <samer.mnr@gmail.com>
2020-03-07 12:39:01 +0100

    password reset component

---
+++ src/components/PasswordReset.js
@@ -0,0 +1,74 @@
+import React, { Component } from "react";
+import { auth } from "../modules/firebase";
+class PasswordReset extends Component {
+    state = {
+        email: "",
+        error: false,
+        errorMessage: ""
+    };
+
+    handleChange = e => {
+        this.setState({
+            [e.target.name]: e.target.value
+        });
+    };
+
+    handleSubmit = e => {
+        e.preventDefault();
+        auth.sendPasswordResetEmail(this.state.email)
+            .then(() => {
+                alert("Password Reset Email Sent!");
+                console.log(this.props);
+                this.setState({
+                    error: false,
+                    errorMessage: ""
+                });
+                return this.props.history.push("/login");
+            })
+            .catch(error => {
+                var errorCode = error.code;
+                var errorMessage = error.message;
+                errorCode === "auth/invalid-email" &&
+                    this.setState({
+                        error: true,
+                        errorMessage: errorMessage
+                    });
+                errorCode === "auth/user-not-found" &&
+                    this.setState({
+                        error: true,
+                        errorMessage: errorMessage
+                    });
+            });
+    };
+
+    render() {
+        const errorDiv = this.state.error ? (
+            <div className="alert alert-warning mt-3" role="alert">
+                {this.state.errorMessage}
+            </div>
+        ) : (
+            ""
+        );
+        return (
+            <form onSubmit={e => this.handleSubmit(e)}>
+                {errorDiv}
+                <div className="form-group">
+                    <label htmlFor="inputEmail">Write your email address</label>
+                    <input
+                        type="email"
+                        className="form-control"
+                        id="inputEmail"
+                        aria-describedby="emailHelp"
+                        name="email"
+                        onChange={e => this.handleChange(e)}
+                    />
+                </div>
+                <button type="submit" className="btn btn-primary">
+                    Submit
+                </button>
+            </form>
+        );
+    }
+}
+
+export default PasswordReset;
