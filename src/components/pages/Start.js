ced5eeed4a6f3100f22b936d246c1775d8df7124
samer munawwar <samer.mnr@gmail.com>
2020-03-11 13:03:37 +0100

    lägg till spinner

--- src/components/pages/Start.js
+++ src/components/pages/Start.js
@@ -1,79 +1,85 @@
-import React from 'react'
-import QuizCard from '../quiz/QuizCard'
-import { db } from '../../modules/firebase'
+import React from "react";
+import QuizCard from "../quiz/QuizCard";
+import { db } from "../../modules/firebase";
 
 class Start extends React.Component {
-	state = {
-		quiz: null
-	}
+    state = {
+        quiz: null
+    };
 
-	componentDidMount() {
-		this.gitData()
-	}
+    componentDidMount() {
+        this.gitData();
+    }
 
-	// Get all quizzes from Firebase
-	gitData = () => {
-		db.collection('quizzes')
-			.get()
-			.then(response => {
-				const quiz = []
-				response.forEach(doc => {
-					if (!doc.data().quiz) {
-						return
-					}
-					quiz.push({
-						id: doc.id,
-						...doc.data()
-					})
-				})
-				this.setState({
-					quiz
-				})
-			})
-	}
+    // Get all quizzes from Firebase
+    gitData = () => {
+        db.collection("quizzes")
+            .get()
+            .then(response => {
+                const quiz = [];
+                response.forEach(doc => {
+                    if (!doc.data().quiz) {
+                        return;
+                    }
+                    quiz.push({
+                        id: doc.id,
+                        ...doc.data()
+                    });
+                });
+                this.setState({
+                    quiz
+                });
+            });
+    };
 
-	// Delete quiz from Firebase
-	deleteQuiz = id => {
-		db.collection('quizzes')
-			.doc(id)
-			.delete()
-			.then(() => {
-				this.gitData()
-			})
-			.catch(error => {
-				console.error('Error removing document: ', error)
-			})
-	}
+    // Delete quiz from Firebase
+    deleteQuiz = id => {
+        db.collection("quizzes")
+            .doc(id)
+            .delete()
+            .then(() => {
+                this.gitData();
+            })
+            .catch(error => {
+                console.error("Error removing document: ", error);
+            });
+    };
 
-	render() {
-		const quizzez = this.state.quiz
-			? this.state.quiz.map((quiz, i) => {
-					return (
-						<QuizCard
-							quiz={quiz}
-							key={i}
-							deleteQuiz={this.deleteQuiz}
-							user={this.props.user}
-						/>
-					)
-			  })
-			: ''
+    render() {
+        const quizzez = this.state.quiz
+            ? this.state.quiz.map((quiz, i) => {
+                  return (
+                      <QuizCard
+                          quiz={quiz}
+                          key={i}
+                          deleteQuiz={this.deleteQuiz}
+                          user={this.props.user}
+                      />
+                  );
+              })
+            : "";
 
-		return (
-			<div>
-				<h1 className='text-center mb-5'>Quizzes</h1>
-				<div className='row'>{quizzez}</div>
-				{!this.props.user ? (
-					<p className='text-center mt-4'>
-						If you want to add a new quiz, please go to sign up and register
-						with your email
-					</p>
-				) : (
-					''
-				)}
-			</div>
-		)
-	}
+        return !this.state.quiz ? (
+            <div class="d-flex justify-content-center">
+                <div class="spinner-border" role="status">
+                    <span class="sr-only">Loading...</span>
+                </div>
+            </div>
+        ) : (
+            <div>
+                <h1 className="text-center mb-5">Quizzes</h1>
+                <div className="row">{quizzez}</div>
+                {!this.props.user ? (
+                    <p className="text-center mt-4">
+                        If you want to add a new quiz, please go to sign up and
+                        register with your email
+                    </p>
+                ) : (
+                    ""
+                )}
+            </div>
+        );
+    }
 }
 
-export default Start
+export default Start;
