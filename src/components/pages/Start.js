d7295a6b450452963dd2da53a26f7ad5a9e6ea83
samer munawwar <samer.mnr@gmail.com>
2020-03-11 13:15:35 +0100

    ändrade class till className

--- src/components/pages/Start.js
+++ src/components/pages/Start.js
@@ -60,9 +60,9 @@ class Start extends React.Component {
             : "";
 
         return !this.state.quiz ? (
-            <div class="d-flex justify-content-center">
-                <div class="spinner-border" role="status">
-                    <span class="sr-only">Loading...</span>
+            <div className="d-flex justify-content-center">
+                <div className="spinner-border" role="status">
+                    <span className="sr-only">Loading...</span>
                 </div>
             </div>
         ) : (
