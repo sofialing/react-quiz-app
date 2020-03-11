c2eab73f882cfb58a6fbb74c75b2c940e41a49b1
Sofia Ling <sofialing@live.se>
2020-03-11 11:21:19 +0100

    Cleaned Navbar code

--- src/components/navigation/Navbar.js
+++ src/components/navigation/Navbar.js
@@ -8,58 +8,46 @@ const Navbar = props => {
 			<Link to='/' className='navbar-brand'>
 				Quiz
 			</Link>
-			<button
-				className='navbar-toggler'
-				type='button'
-				data-toggle='collapse'
-				data-target='#navbarNav'
-				aria-controls='navbarNav'
-				aria-expanded='false'
-				aria-label='Toggle navigation'>
-				<span className='navbar-toggler-icon'></span>
-			</button>
-			<div className='collapse navbar-collapse' id='navbarNav'>
-				<ul className='navbar-nav ml-auto'>
+			<ul className='navbar-nav ml-auto'>
+				<li className='nav-item'>
+					<NavLink to='/' className='nav-link'>
+						All Quizzes
+					</NavLink>
+				</li>
+				{props.user ? (
 					<li className='nav-item'>
-						<NavLink to='/' className='nav-link'>
-							All Quizzes
+						<NavLink to='/add-quiz' className='nav-link'>
+							Add Quiz
 						</NavLink>
 					</li>
-					{props.user ? (
+				) : (
+					''
+				)}
+
+				{props.user ? (
+					<li className='nav-item'>
+						<Link
+							to='/'
+							className='nav-link logout'
+							onClick={() => auth.signOut()}>
+							Logout
+						</Link>
+					</li>
+				) : (
+					<React.Fragment>
 						<li className='nav-item'>
-							<NavLink to='/add-quiz' className='nav-link'>
-								Add Quiz
+							<NavLink to='/login' className='nav-link'>
+								Login
 							</NavLink>
 						</li>
-					) : (
-						''
-					)}
-
-					{props.user ? (
 						<li className='nav-item'>
-							<Link
-								to='/'
-								className='nav-link logout'
-								onClick={() => auth.signOut()}>
-								Logout
-							</Link>
+							<NavLink to='/sign-up' className='nav-link'>
+								Sign up
+							</NavLink>
 						</li>
-					) : (
-						<React.Fragment>
-							<li className='nav-item'>
-								<NavLink to='/login' className='nav-link'>
-									Login
-								</NavLink>
-							</li>
-							<li className='nav-item'>
-								<NavLink to='/sign-up' className='nav-link'>
-									Sign up
-								</NavLink>
-							</li>
-						</React.Fragment>
-					)}
-				</ul>
-			</div>
+					</React.Fragment>
+				)}
+			</ul>
 		</nav>
 	)
 }
