2601eabef2489b91460c3bc999b9762092154666
samer munawwar <samer.mnr@gmail.com>
2020-03-11 11:04:32 +0100

    lägg till question poing

--- src/components/pages/Result.js
+++ src/components/pages/Result.js
@@ -1,42 +1,56 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
+import React from "react";
+import { Link } from "react-router-dom";
 
 const Result = props => {
-	const resultMsg =
-		props.result.score === props.result.maxScore ? (
-			<div className='alert alert-light py-5 text-center' role='alert'>
-				<h2 className='alert-heading'>
-					Congratulations{' '}
-					<span role='img' aria-label='jsx-a11y/accessible-emoji'>
-						🥳
-					</span>
-				</h2>
-				<h3>
-					Your result on the quiz:{' '}
-					<span className='input-result'>{props.result.name}</span> was{' '}
-					<span className='input-result'>{props.result.score}</span> out of{' '}
-					<span className='input-result'>{props.result.maxScore}</span>
-				</h3>
-			</div>
-		) : (
-			<div className='alert alert-light py-5 text-center' role='alert'>
-				<h2 className='alert-heading'>Good job!</h2>
-				<h3>
-					Your result on the quiz:{' '}
-					<span className='input-result'>{props.result.name}</span> was{' '}
-					<span className='input-result'>{props.result.score}</span> out of{' '}
-					<span className='input-result'>{props.result.maxScore}</span>
-				</h3>
-			</div>
-		)
-	return (
-		<div className='d-flex justify-content-center align-items-center flex-column'>
-			{resultMsg}
-			<Link to='/' className='btn btn-primary'>
-				Back to Quizzes
-			</Link>
-		</div>
-	)
-}
+    const resultMsg =
+        props.result.score === props.result.maxScore ? (
+            <div className="alert alert-light py-5 text-center" role="alert">
+                <h2 className="alert-heading">
+                    Congratulations{" "}
+                    <span role="img" aria-label="jsx-a11y/accessible-emoji">
+                        🥳
+                    </span>
+                </h2>
+                <h3>
+                    Your result on the quiz:{" "}
+                    <span className="input-result">{props.result.name}</span>{" "}
+                    was{" "}
+                    <span className="input-result">{props.result.score}</span>{" "}
+                    out of{" "}
+                    <span className="input-result">
+                        {props.result.maxScore}
+                    </span>
+                </h3>
+            </div>
+        ) : (
+            <div className="alert alert-light py-5 text-center" role="alert">
+                <h2 className="alert-heading">Good job!</h2>
+                <h3>
+                    Your result on the quiz:{" "}
+                    <span className="input-result">{props.result.name}</span>{" "}
+                    was{" "}
+                    <span className="input-result">{props.result.score}</span>{" "}
+                    out of{" "}
+                    <span className="input-result">
+                        {props.result.maxScore}
+                    </span>
+                </h3>
+            </div>
+        );
+    return (
+        <React.Fragment>
+            <div className="d-flex justify-content-center align-items-center flex-column">
+                {resultMsg}
+                <Link to="/" className="btn btn-primary">
+                    Back to Quizzes
+                </Link>
+            </div>
+            <div className="mt-4">
+                <h1>The correct answers are:</h1>
+                <ul className='class="list-group'>{props.correctAnswers()}</ul>
+            </div>
+        </React.Fragment>
+    );
+};
 
-export default Result
+export default Result;
--- src/components/quiz/QuizQuestion.js
+++ src/components/quiz/QuizQuestion.js
@@ -1,27 +1,29 @@
-import React from 'react'
-import QuizOptions from './QuizOptions'
+import React from "react";
+import QuizOptions from "./QuizOptions";
 
 const QuizQuestion = props => {
-	const { question, correct, wrong, point } = props.quiz
-	const options = [...correct, ...wrong]
-	options.sort(() => 0.5 - Math.random())
+    const { question, correct, wrong, point } = props.quiz;
+    const options = [...correct, ...wrong];
+    options.sort(() => 0.5 - Math.random());
 
-	return (
-		<div className='card mb-4'>
-			<div className='card-body'>
-				<h5 className='card-title'>
-					{props.number}. {question.charAt(0).toUpperCase() + question.slice(1)}
-				</h5>
-				<QuizOptions
-					options={options}
-					correct={correct}
-					onUpdateScore={props.onUpdateScore}
-					point={point}
-					isLastQuestion={props.isLastQuestion}
-				/>
-			</div>
-		</div>
-	)
-}
+    return (
+        <div className="card mb-4">
+            <div className="card-body">
+                <h5 className="card-title">
+                    {props.number}.{" "}
+                    {question.charAt(0).toUpperCase() + question.slice(1)}{" "}
+                    <small>{point} points</small>
+                </h5>
+                <QuizOptions
+                    options={options}
+                    correct={correct}
+                    onUpdateScore={props.onUpdateScore}
+                    point={point}
+                    isLastQuestion={props.isLastQuestion}
+                />
+            </div>
+        </div>
+    );
+};
 
-export default QuizQuestion
+export default QuizQuestion;
